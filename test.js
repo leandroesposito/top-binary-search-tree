2445845afb2253ea1365fc7a0d1d181ff5309e9a
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-01 19:32:55 -0300

    Add test for delete node

--- test.js
+++ test.js
@@ -1,9 +1,20 @@
 import BinarySearchTree from "./BinarySearchTree.js";
 
 function insertAndPrint(binarySearchTree, data) {
-  const [parent, node] = binarySearchTree.findParentAndNodeOf(data);
+  const [parent, node] = BinarySearchTree.findParentAndNodeOf(
+    binarySearchTree.root,
+    data
+  );
   console.log("parent of", data, "is", parent, "current is", node);
   binarySearchTree.insert(data);
+  BinarySearchTree.prettyPrint(binarySearchTree.root);
+}
+
+function removeAndPrint(binarySearchTree, data) {
+  console.log("before");
+  BinarySearchTree.prettyPrint(binarySearchTree.root);
+  console.log("after removing", data);
+  binarySearchTree.deleteItem(data);
   BinarySearchTree.prettyPrint(binarySearchTree.root);
 }
 
@@ -18,3 +29,12 @@ insertAndPrint(binarySearchTree, 75);
 insertAndPrint(binarySearchTree, 10);
 insertAndPrint(binarySearchTree, 20);
 insertAndPrint(binarySearchTree, 10);
+
+removeAndPrint(binarySearchTree, 8);
+removeAndPrint(binarySearchTree, 67);
+removeAndPrint(binarySearchTree, 324);
+removeAndPrint(binarySearchTree, 9);
+removeAndPrint(binarySearchTree, 4);
+removeAndPrint(binarySearchTree, 23);
+removeAndPrint(binarySearchTree, 6345);
+removeAndPrint(binarySearchTree, 200);
